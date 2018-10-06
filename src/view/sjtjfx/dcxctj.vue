24897cb3874827a44c0799feb6be4b0afab47092
yelingkong <zhaowanjun123@gmail.com>
2018-10-06 12:41:14 +0800

    说明

--- src/view/sjtjfx/dcxctj.vue
+++ src/view/sjtjfx/dcxctj.vue
@@ -1,48 +1,139 @@
 <template>
   <div>
-    <div class="dcxctj">
-      <Card class="search">
-        <Row>
+    <Row class="search_list">
+      <Row :gutter="16" class="search_list2">
+        <Form :label-width="100">
           <i-col span="8">
-            <Form :label-width="80">
-              <FormItem label="地区：">
-                <Cascader :data="data" trigger="hover" transfer></Cascader>
-              </FormItem>
-            </Form>
+            <FormItem label="地区：">
+              <Cascader :data="data" trigger="hover" transfer></Cascader>
+            </FormItem>
           </i-col>
           <i-col span="8">
-            <Form :label-width="80">
-              <FormItem label="巡查日期：">
-                <DatePicker :value="countDate" type="daterange" placement="bottom-end"
-                            style="width: 100%" transfer></DatePicker>
-              </FormItem>
-            </Form>
+            <FormItem label="日期：">
+              <DatePicker :value="countDate" type="daterange" placement="bottom-end"
+                          style="width: 100%" transfer></DatePicker>
+            </FormItem>
           </i-col>
           <i-col span="3" offset="21">
             <i-col span="12">
-              <Button type="primary" size="large">查询</Button>
+              <Button type="primary" size="large" class="fr">查询</Button>
             </i-col>
             <i-col span="12">
-              <Button size="large">重置</Button>
+              <Button size="large" class="fr">重置</Button>
             </i-col>
           </i-col>
+        </Form>
+      </Row>
+    </Row>
+    <Card title="常州市督查巡查统计" :padding="0" shadow class="syztfb sycppzfb">
+      <Button type="primary" size="large" slot="extra" class="dcsj">导出数据</Button>
+      <div class="list_table">
+        <Row :gutter="16" class="search_list2">
+          <Col>
+            <tables ref="tables" editable v-model="tableData" :columns="columns"/>
+          </Col>
         </Row>
-      </Card>
-      <Card title="常州市督查巡查统计" shadow class="xuncha_tongjibiao">
-        <Button class="exportdata" slot="extra" type="primary" size="large" @click="exportData(1)">导出数据</Button>
-        <template>
-          <Table :columns="columns1" :data="data1" ref="table"></Table>
-        </template>
-      </Card>
-    </div>
+        <Row :gutter="16" type="flex" justify="end">
+          <Col span="24" justify="end" class="page_right">
+            <Page :total="100"/>
+          </Col>
+          <Col span="1"></Col>
+        </Row>
+      </div>
+    </Card>
   </div>
 </template>
-
 <script>
+  import Tables from '_c/tables'
+  import {getTableData} from '@/api/data'
+
   export default {
-    name: "dcxctj",
+    name: 'zcrzsh',
+    components: {
+      Tables
+    },
     data() {
       return {
+        listztHover: 0,
+        listzt: [
+          {
+            name: '合计主体数',
+            num: 1332
+          },
+          {
+            name: '企业/个体工商',
+            num: 322
+          },
+          {
+            name: '合作社',
+            num: 352
+          },
+          {
+            name: '家庭农场',
+            num: 24
+          },
+          {
+            name: '个人',
+            num: 24
+          }
+        ],
+        reviewStatus: [
+          {
+            value: '',
+            label: '全部'
+          },
+          {
+            value: '1',
+            label: '待审核'
+          },
+          {
+            value: '2',
+            label: '待复审'
+          },
+          {
+            value: '3',
+            label: '已复审'
+          }
+        ],
+        enterprises: [
+          {
+            value: '0',
+            label: '全部'
+          },
+          {
+            value: '1',
+            label: '是'
+          },
+          {
+            value: '2',
+            label: '否'
+          }
+        ],
+        organizationalForm: [
+          {
+            value: '0',
+            label: '全部'
+          },
+          {
+            value: '1',
+            label: '企业/个体工商户'
+          },
+          {
+            value: '2',
+            label: '合作社'
+          },
+          {
+            value: '3',
+            label: '家庭农场'
+          },
+          {
+            value: '4',
+            label: '个人'
+          }
+        ],
+        organizationalFormModel: '0',
+        reviewStatusModel: '0',
+        enterprisesModel: '0',
         countDate: [],
         form: {
           mail: '',
@@ -52,103 +143,134 @@
           zone: '010',
           phone: '1000000'
         },
-        columns1: [
+        columns: [
+          {type: 'selection', width: 60, align: 'center'},
+          {title: '地区', key: 'address', sortable: true},
+          {title: '主体名称', key: 'zhuti',},
+          {title: '注册时间', key: 'createTime',},
           {
-            title: '地区',
-            key: 'name'
+            title: '审核状态',
+            key: 'reviewTheStatus',
+            render: (h, {row}) => {
+              if (row.reviewTheStatus === 0) {
+                return h('Badge', {
+                  props: {
+                    status: 'default',
+                    text: '待审核'
+                  }
+                })
+              } else if (row.reviewTheStatus === 1) {
+                return h('Badge', {
+                  props: {
+                    status: 'error',
+                    text: '待复审'
+                  }
+                })
+              } else if (row.reviewTheStatus === 2) {
+                return h('Badge', {
+                  props: {
+                    status: 'success',
+                    text: '已复审'
+                  }
+                })
+              }
+            }
           },
           {
-            title: '未巡查企业数（家)',
-            key: 'key1'
+            title: '审核日期',
+            key: 'dateOfAudit',
+            render: (h, {row}) => {
+              if (row.reviewTheStatus === 0) {
+                return h('div', {})
+              } else {
+                return h('div', {}, row.dateOfAudit)
+              }
+            }
           },
           {
-            title: '巡查1次企业数（家）',
-            key: 'key2'
-          },
-          {
-            title: '巡查2次及以上企业数（家）',
-            key: 'key3'
-          },
-          {
-            title: '巡查主体总数（家）',
-            key: 'key4'
-          },
-          {
-            title: '巡查覆盖率（%）',
-            key: 'key5'
-          },
-          {
-            title: '巡查合格率（%）',
-            key: 'key6'
+            title: '操作',
+            key: 'action',
+            width: 150,
+            align: 'center',
+            render: (h, {row, index}) => {
+              if (row.reviewTheStatus === 0) {
+                return h('div', [
+                  h('Button', {
+                    props: {
+                      type: 'primary',
+                      size: 'small'
+                    },
+                    style: {
+                      marginRight: '5px'
+                    },
+                    on: {
+                      click: () => {
+                        this.show(row.index)
+                      }
+                    }
+                  }, '查看'),
+                  h('Button', {
+                    props: {
+                      type: 'error',
+                      size: 'small'
+                    },
+                    on: {
+                      click: () => {
+                        this.shenhe(index)
+                      }
+                    }
+                  }, '审核')
+                ])
+              } else if (row.reviewTheStatus === 1) {
+                return h('div', [
+                  h('Button', {
+                    props: {
+                      type: 'primary',
+                      size: 'small'
+                    },
+                    style: {
+                      marginRight: '5px'
+                    },
+                    on: {
+                      click: () => {
+                        this.show(index)
+                      }
+                    }
+                  }, '查看'),
+                  h('Button', {
+                    props: {
+                      type: 'error',
+                      size: 'small'
+                    },
+                    on: {
+                      click: () => {
+                        this.fushen(index)
+                      }
+                    }
+                  }, '复审')
+                ])
+              } else if (row.reviewTheStatus === 2) {
+                return h('div', [
+                  h('Button', {
+                    props: {
+                      type: 'primary',
+                      size: 'small'
+                    },
+                    style: {
+                      marginRight: '5px'
+                    },
+                    on: {
+                      click: () => {
+                        this.show(index)
+                      }
+                    }
+                  }, '查看')
+                ])
+              }
+            }
           }
         ],
-        data1: [
-          {
-            name: '天宁区',
-            key: 10000,
-            key1: 10000,
-            key2: 10000,
-            key3: 10000,
-            key4: 10000,
-            key5: 10000,
-            key6: 10000,
-          }, {
-            name: '天宁区',
-            key: 10000,
-            key1: 10000,
-            key2: 10000,
-            key3: 10000,
-            key4: 10000,
-            key5: 10000,
-            key6: 10000,
-          }, {
-            name: '天宁区',
-            key: 10000,
-            key1: 10000,
-            key2: 10000,
-            key3: 10000,
-            key4: 10000,
-            key5: 10000,
-            key6: 10000,
-          }, {
-            name: '天宁区',
-            key: 10000,
-            key1: 10000,
-            key2: 10000,
-            key3: 10000,
-            key4: 10000,
-            key5: 10000,
-            key6: 10000,
-          }, {
-            name: '天宁区',
-            key: 10000,
-            key1: 10000,
-            key2: 10000,
-            key3: 10000,
-            key4: 10000,
-            key5: 10000,
-            key6: 10000,
-          }, {
-            name: '天宁区',
-            key: 10000,
-            key1: 10000,
-            key2: 10000,
-            key3: 10000,
-            key4: 10000,
-            key5: 10000,
-            key6: 10000,
-          }, {
-            name: '天宁区',
-            key: 10000,
-            key1: 10000,
-            key2: 10000,
-            key3: 10000,
-            key4: 10000,
-            key5: 10000,
-            key6: 10000,
-          },
-
-        ],
+        tableData: [],
         data: [{
           value: 'beijing',
           label: '北京',
@@ -198,28 +320,116 @@
         }]
       }
     },
-    mounted: function () {
-      this.hanleSetDate();
+    mounted() {
+      getTableData().then(res => {
+        this.tableData = res.data
+      })
     },
     methods: {
-      exportData(type) {
-        this.$refs.table.exportCsv({
-          filename: 'The original data'
-        });
+      show(index) {
+        this.$Modal.info({
+          title: 'User Info',
+          content: `Name：${this.tableData[index].name}<br>Age：${this.tableData[index].age}<br>Address：${this.tableData[index].address}`
+        })
       },
-      hanleSetDate() {
-        const today = (new Date()).getTime();
-        let date = (new Date()).getTime();
-        date.setData(1);
-        date.setMonth(0);
-        this.countDate = [(new Date(date)), (new Date(today))]
+      shenhe(index) {
+        this.tableData[index].reviewTheStatus = 1
+      },
+      fushen(index) {
+        this.tableData[index].reviewTheStatus = 2
       }
-
     }
   }
-</script>
 
-<style scoped>
+</script>
+<style lang="less">
+  .count-style {
+    font-size: 50px;
+  }
+
+  .list-zt-body {
+    background: #fff;
+    margin-bottom: 20px;
+  }
+
+  .list-zt {
+    text-align: center;
+    margin: 25px auto;
+    box-sizing: border-box;
+    border-right: 1px solid #E9E9E9;
+    cursor: pointer;
+
+  }
+
+  .list-zt p {
+    color: #272727;
+    font-size: 24px;
+    line-height: 45px
+  }
+
+  .list-zt span {
+    color: #969696;
+    font-size: 14px
+  }
+
+  .list-zt.hover p, .list-zt.hover span {
+    color: #48A1FF
+  }
+
+  .list-zt.hover {
+    position: relative
+  }
+
+  .list-zt.hover .list-zt-top {
+    position: absolute;
+    top: -31px;
+    width: 100%;
+    background: #72B2FC;
+    height: 6px;
+    display: none;
+  }
+
+  .list-zt.hover .list-zt-top::before {
+    content: "";
+    position: absolute;
+    width: 0;
+    height: 0;
+    border-right: 10px solid transparent;
+    border-left: 10px solid transparent;
+    border-top: 10px solid #72B2FC;
+    z-index: 99;
+    bottom: -10px;
+    left: 50%;
+    margin-left: -10px
+  }
+
+  .list-zt.hover .list-zt-bottom {
+    position: absolute;
+    bottom: -31px;
+    width: 100%;
+    background: #72B2FC;
+    height: 6px;
+    display: none;
+  }
+
+  .list-zt.hover .list-zt-bottom::before {
+    content: "";
+    position: absolute;
+    width: 0;
+    height: 0;
+    border-right: 10px solid transparent;
+    border-left: 10px solid transparent;
+    border-bottom: 10px solid #72B2FC;
+    z-index: 99;
+    top: -10px;
+    left: 50%;
+    margin-left: -10px
+  }
+
+  .list-mt20 {
+    margin: 20px auto
+  }
+
   .xuncha_tongjibiao {
     text-align: left;
   }
@@ -234,6 +444,48 @@
     text-align: left;
     padding-left: 20px;
   }
-  .dcxctj{padding: 16px;background: #F5F7F9;}
-  .search{margin-bottom: 20px;}
+
+  .dcxctj {
+    padding: 16px;
+  }
+
+  .search_list {
+    margin-top: 0px;
+    background: #fff;
+    margin-bottom: 20px;
+  }
+
+  .list_table {
+    background: #fff;
+  }
+
+  .search_list2 {
+    margin: 10px !important;
+  }
+
+  .fr {
+    float: right;
+  }
+
+  .page_right {
+    text-align: right;
+    margin-right: 17px;
+    margin-bottom: 20px;
+    margin-top: 20px;
+  }
+
+  .fl {
+    float: left;
+  }
+
+  .button_left {
+    margin-left: 10px !important;
+  }
+
+  .list-zt-5 {
+    width: 20%;
+    float: left;
+  }
+  .dcsj{margin-top: -5px;}
 </style>
+
